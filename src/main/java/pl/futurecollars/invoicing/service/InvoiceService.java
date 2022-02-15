bc13eaef3e25627888b47cf429f134c7af677563
Kate <56678831+katdunaj@users.noreply.github.com>
2022-02-15 15:16:42 +0100

    add RestService, no tests (#4)
    Co-authored-by: katdunaj@gmail.com <katarzyna.ruda112@gmail.com>

--- src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -1,13 +1,52 @@
 package pl.futurecollars.invoicing.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
-import pl.futurecollars.invoicing.db.Database;
+import java.util.stream.Collectors;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import pl.futurecollars.invoicing.db.InMemoryDatabase;
 import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.model.InvoiceEntry;
 
+@Component
+@Data
 public class InvoiceService {
 
-  private Database database;
+  private final InMemoryDatabase database;
+
+  public BigDecimal getTotalNet(Invoice invoice) {
+    return invoice.getInvoiceEntries()
+      .stream()
+      .map(InvoiceEntry::getPrice)
+      .reduce(BigDecimal.valueOf(0), BigDecimal::add);
+  }
+
+  public BigDecimal getTotalTaxValue(Invoice invoice) {
+    return invoice.getInvoiceEntries()
+      .stream()
+      .map(InvoiceEntry::getPrice)
+      .reduce(BigDecimal.valueOf(0), BigDecimal::add);
+  }
+
+  public BigDecimal getTotalGross(Invoice invoice) {
+    return getTotalNet(invoice).add(getTotalTaxValue(invoice));
+  }
+
+  public List<Invoice> filterByIssuer(String issuer) {
+    return database.getAll()
+      .stream()
+      .filter((Invoice invoice) -> issuer.equals(invoice.getIssuer().getName()))
+        .collect(Collectors.toList());
+  }
+
+  public List<Invoice> filterByReceiver(String receiver) {
+    return database.getAll()
+      .stream()
+      .filter((Invoice invoice) -> receiver.equals(invoice.getReceiver().getName()))
+        .collect(Collectors.toList());
+  }
 
   public Invoice save(Invoice invoice) {
     return database.save(invoice);
