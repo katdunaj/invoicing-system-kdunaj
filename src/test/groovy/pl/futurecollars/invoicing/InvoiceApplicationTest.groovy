69c1e3c9a7088832a9dcb5704c97c0135ae2350b
Kate <56678831+katdunaj@users.noreply.github.com>
2022-04-02 08:50:20 +0200

    upgrade version 3 (#8)
    * upgrade version 3
    
    * first version with swagger
    
    * Configured Swagger
    
    Co-authored-by: katdunaj@gmail.com <katarzyna.ruda112@gmail.com>

--- src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
@@ -2,10 +2,10 @@ package pl.futurecollars.invoicing
 
 import spock.lang.Specification
 
-class AppTest extends Specification {
+class InvoiceApplicationTest extends Specification {
     def "application has a greeting"() {
         setup:
-        def app = new App()
+        def app = new InvoiceApplication()
 
         when:
         def result = app.greeting
